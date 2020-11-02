550bc58ed525fee35055213f029a0b8f47d04d19
Mahmudunnabi Rain <mahmudunnabirain@gmail.com>
2020-11-02 19:27:57 +0600

    Added bacground image, File handling, sign up and login features implemented

--- WindowsFormsApp/LoginForm.Designer.cs
+++ WindowsFormsApp/LoginForm.Designer.cs
@@ -28,95 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Welcome_label = new System.Windows.Forms.Label();
-            this.main_password_input = new System.Windows.Forms.TextBox();
-            this.Login_submit = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.signup_password_input = new System.Windows.Forms.TextBox();
+            this.signup_submit = new System.Windows.Forms.Button();
             this.instruction_label = new System.Windows.Forms.Label();
+            this.signup_panel = new System.Windows.Forms.Panel();
+            this.checkBox_signup_show_pass = new System.Windows.Forms.CheckBox();
+            this.login_panel = new System.Windows.Forms.Panel();
+            this.checkBox_show_login_pass = new System.Windows.Forms.CheckBox();
+            this.login_label = new System.Windows.Forms.Label();
+            this.login_pass_input = new System.Windows.Forms.TextBox();
+            this.signup_panel.SuspendLayout();
+            this.login_panel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Welcome_label
+            // signup_password_input
             // 
-            this.Welcome_label.AutoSize = true;
-            this.Welcome_label.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Welcome_label.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.Welcome_label.Location = new System.Drawing.Point(336, 46);
-            this.Welcome_label.Name = "Welcome_label";
-            this.Welcome_label.Size = new System.Drawing.Size(142, 24);
-            this.Welcome_label.TabIndex = 0;
-            this.Welcome_label.Text = "Welcome Sir";
+            this.signup_password_input.BackColor = System.Drawing.Color.Black;
+            this.signup_password_input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.signup_password_input.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signup_password_input.ForeColor = System.Drawing.Color.GhostWhite;
+            this.signup_password_input.Location = new System.Drawing.Point(79, 142);
+            this.signup_password_input.MaxLength = 10;
+            this.signup_password_input.Name = "signup_password_input";
+            this.signup_password_input.Size = new System.Drawing.Size(287, 36);
+            this.signup_password_input.TabIndex = 1;
+            this.signup_password_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.signup_password_input.UseSystemPasswordChar = true;
+            this.signup_password_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_password_input_KeyDown);
+            this.signup_password_input.Validating += new System.ComponentModel.CancelEventHandler(this.Main_password_input_Validating);
             // 
-            // main_password_input
+            // signup_submit
             // 
-            this.main_password_input.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.main_password_input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.main_password_input.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.main_password_input.ForeColor = System.Drawing.Color.GhostWhite;
-            this.main_password_input.Location = new System.Drawing.Point(258, 136);
-            this.main_password_input.MaxLength = 10;
-            this.main_password_input.Name = "main_password_input";
-            this.main_password_input.Size = new System.Drawing.Size(287, 36);
-            this.main_password_input.TabIndex = 1;
-            this.main_password_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.main_password_input.TextChanged += new System.EventHandler(this.Main_password_input_TextChanged);
-            this.main_password_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_password_input_KeyDown);
-            this.main_password_input.Validating += new System.ComponentModel.CancelEventHandler(this.Main_password_input_Validating);
-            // 
-            // Login_submit
-            // 
-            this.Login_submit.BackColor = System.Drawing.Color.DodgerBlue;
-            this.Login_submit.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Login_submit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Login_submit.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Login_submit.ForeColor = System.Drawing.Color.GhostWhite;
-            this.Login_submit.Location = new System.Drawing.Point(258, 180);
-            this.Login_submit.Name = "Login_submit";
-            this.Login_submit.Size = new System.Drawing.Size(287, 33);
-            this.Login_submit.TabIndex = 2;
-            this.Login_submit.Text = "SUBMIT";
-            this.Login_submit.UseVisualStyleBackColor = false;
-            this.Login_submit.Click += new System.EventHandler(this.Login_submit_Click);
+            this.signup_submit.BackColor = System.Drawing.Color.DarkGreen;
+            this.signup_submit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signup_submit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signup_submit.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signup_submit.ForeColor = System.Drawing.Color.GhostWhite;
+            this.signup_submit.Location = new System.Drawing.Point(79, 188);
+            this.signup_submit.Name = "signup_submit";
+            this.signup_submit.Size = new System.Drawing.Size(287, 33);
+            this.signup_submit.TabIndex = 2;
+            this.signup_submit.Text = "SUBMIT";
+            this.signup_submit.UseVisualStyleBackColor = false;
+            this.signup_submit.Click += new System.EventHandler(this.Signup_submit_Click);
             // 
             // instruction_label
             // 
             this.instruction_label.AutoSize = true;
             this.instruction_label.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.instruction_label.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.instruction_label.Location = new System.Drawing.Point(290, 102);
+            this.instruction_label.Location = new System.Drawing.Point(140, 108);
             this.instruction_label.Name = "instruction_label";
-            this.instruction_label.Size = new System.Drawing.Size(226, 24);
+            this.instruction_label.Size = new System.Drawing.Size(166, 24);
             this.instruction_label.TabIndex = 3;
-            this.instruction_label.Text = "Set a new password";
+            this.instruction_label.Text = "Set a new PIN";
+            // 
+            // signup_panel
+            // 
+            this.signup_panel.BackColor = System.Drawing.Color.Transparent;
+            this.signup_panel.Controls.Add(this.checkBox_signup_show_pass);
+            this.signup_panel.Controls.Add(this.instruction_label);
+            this.signup_panel.Controls.Add(this.signup_password_input);
+            this.signup_panel.Controls.Add(this.signup_submit);
+            this.signup_panel.Location = new System.Drawing.Point(169, 12);
+            this.signup_panel.Name = "signup_panel";
+            this.signup_panel.Size = new System.Drawing.Size(437, 337);
+            this.signup_panel.TabIndex = 4;
+            this.signup_panel.Visible = false;
+            // 
+            // checkBox_signup_show_pass
+            // 
+            this.checkBox_signup_show_pass.AutoSize = true;
+            this.checkBox_signup_show_pass.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox_signup_show_pass.ForeColor = System.Drawing.SystemColors.Info;
+            this.checkBox_signup_show_pass.Location = new System.Drawing.Point(372, 148);
+            this.checkBox_signup_show_pass.Name = "checkBox_signup_show_pass";
+            this.checkBox_signup_show_pass.Size = new System.Drawing.Size(61, 23);
+            this.checkBox_signup_show_pass.TabIndex = 4;
+            this.checkBox_signup_show_pass.Text = "Show";
+            this.checkBox_signup_show_pass.UseVisualStyleBackColor = true;
+            this.checkBox_signup_show_pass.CheckedChanged += new System.EventHandler(this.CheckBox_signup_show_pass_CheckedChanged);
+            // 
+            // login_panel
+            // 
+            this.login_panel.BackColor = System.Drawing.Color.Transparent;
+            this.login_panel.Controls.Add(this.checkBox_show_login_pass);
+            this.login_panel.Controls.Add(this.login_label);
+            this.login_panel.Controls.Add(this.login_pass_input);
+            this.login_panel.Location = new System.Drawing.Point(169, 12);
+            this.login_panel.Name = "login_panel";
+            this.login_panel.Size = new System.Drawing.Size(437, 337);
+            this.login_panel.TabIndex = 5;
+            this.login_panel.Visible = false;
+            // 
+            // checkBox_show_login_pass
+            // 
+            this.checkBox_show_login_pass.AutoSize = true;
+            this.checkBox_show_login_pass.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox_show_login_pass.ForeColor = System.Drawing.SystemColors.Info;
+            this.checkBox_show_login_pass.Location = new System.Drawing.Point(372, 148);
+            this.checkBox_show_login_pass.Name = "checkBox_show_login_pass";
+            this.checkBox_show_login_pass.Size = new System.Drawing.Size(61, 23);
+            this.checkBox_show_login_pass.TabIndex = 4;
+            this.checkBox_show_login_pass.Text = "Show";
+            this.checkBox_show_login_pass.UseVisualStyleBackColor = true;
+            this.checkBox_show_login_pass.CheckedChanged += new System.EventHandler(this.CheckBox_show_login_pass_CheckedChanged);
+            // 
+            // login_label
+            // 
+            this.login_label.AutoSize = true;
+            this.login_label.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_label.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.login_label.Location = new System.Drawing.Point(188, 108);
+            this.login_label.Name = "login_label";
+            this.login_label.Size = new System.Drawing.Size(70, 24);
+            this.login_label.TabIndex = 3;
+            this.login_label.Text = "Login";
+            // 
+            // login_pass_input
+            // 
+            this.login_pass_input.BackColor = System.Drawing.Color.Black;
+            this.login_pass_input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.login_pass_input.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_pass_input.ForeColor = System.Drawing.Color.GhostWhite;
+            this.login_pass_input.Location = new System.Drawing.Point(79, 142);
+            this.login_pass_input.MaxLength = 10;
+            this.login_pass_input.Name = "login_pass_input";
+            this.login_pass_input.Size = new System.Drawing.Size(287, 36);
+            this.login_pass_input.TabIndex = 1;
+            this.login_pass_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.login_pass_input.UseSystemPasswordChar = true;
+            this.login_pass_input.TextChanged += new System.EventHandler(this.Login_pass_input_TextChanged);
             // 
             // LoginForm
             // 
-            this.AcceptButton = this.Login_submit;
+            this.AcceptButton = this.signup_submit;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(784, 361);
-            this.Controls.Add(this.instruction_label);
-            this.Controls.Add(this.Login_submit);
-            this.Controls.Add(this.main_password_input);
-            this.Controls.Add(this.Welcome_label);
+            this.Controls.Add(this.signup_panel);
+            this.Controls.Add(this.login_panel);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mahmudunnabi Rain";
+            this.signup_panel.ResumeLayout(false);
+            this.signup_panel.PerformLayout();
+            this.login_panel.ResumeLayout(false);
+            this.login_panel.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Welcome_label;
-        private System.Windows.Forms.TextBox main_password_input;
-        private System.Windows.Forms.Button Login_submit;
+        private System.Windows.Forms.TextBox signup_password_input;
+        private System.Windows.Forms.Button signup_submit;
         private System.Windows.Forms.Label instruction_label;
+        private System.Windows.Forms.Panel signup_panel;
+        private System.Windows.Forms.CheckBox checkBox_signup_show_pass;
+        private System.Windows.Forms.Panel login_panel;
+        private System.Windows.Forms.CheckBox checkBox_show_login_pass;
+        private System.Windows.Forms.Label login_label;
+        private System.Windows.Forms.TextBox login_pass_input;
     }
 }
 
--- WindowsFormsApp/LoginForm.cs
+++ WindowsFormsApp/LoginForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,39 +16,113 @@ namespace WindowsFormsApp
         public LoginForm()
         {
             InitializeComponent();
+            CheckFirstTimeSetup();
         }
 
-
-
-        private void Main_password_input_TextChanged(object sender, EventArgs e)
+        private void CheckFirstTimeSetup()
         {
-
+            //checking if core_password.txt is empty that means this is first time setup
+            try
+            {
+                using (StreamReader sr = new StreamReader("core_password.txt"))
+                {
+                    string line = sr.ReadLine();
+                    if(String.IsNullOrWhiteSpace(line))
+                    {
+                        signup_panel.Show();
+                        login_panel.Hide();
+                    }
+                    else
+                    {
+                        signup_panel.Hide();
+                        login_panel.Show();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                signup_panel.Show();
+                login_panel.Hide();
+            }
         }
 
-        private void Login_submit_Click(object sender, EventArgs e)
-        {    
-            if(this.ValidateChildren())
+        private void Signup_submit_Click(object sender, EventArgs e)
+        {
+            if (this.ValidateChildren())
             {
-                MessageBox.Show(main_password_input.Text);
-            }         
+                MessageBox.Show(signup_password_input.Text);
+                using (StreamWriter sw = new StreamWriter("core_password.txt"))
+                {
+                    sw.WriteLine(signup_password_input.Text);
+                }
+                signup_panel.Hide();
+            }
         }
 
         private void Main_password_input_KeyDown(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.Enter)
             {
-                Login_submit_Click(sender, e);
+                Signup_submit_Click(sender, e);
             }
         }
 
+      
+
         private void Main_password_input_Validating(object sender, CancelEventArgs e)
         {
-            string password = main_password_input.Text;
+            string password = signup_password_input.Text;
             if (String.IsNullOrWhiteSpace(password))
             {
                 MessageBox.Show("you must enter a password");
                 e.Cancel = true;
                
+            }
+        }
+
+        private void CheckBox_signup_show_pass_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox_signup_show_pass.Checked)
+            {
+                signup_password_input.UseSystemPasswordChar = false;
+                signup_password_input.Focus();
+            }
+            else
+            {
+                signup_password_input.UseSystemPasswordChar = true;
+                signup_password_input.Focus();
+            }
+        }
+
+
+        private void CheckBox_show_login_pass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_show_login_pass.Checked)
+            {
+                login_pass_input.UseSystemPasswordChar = false;
+                login_pass_input.Focus();
+            }
+            else
+            {
+                login_pass_input.UseSystemPasswordChar = true;
+                login_pass_input.Focus();
+            }
+        }
+
+        private void Login_pass_input_TextChanged(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("core_password.txt"))
+            {
+                string line = sr.ReadLine();
+                if (line == login_pass_input.Text)
+                {
+                    login_panel.Hide();
+
+                }
+                
             }
         }
     }
